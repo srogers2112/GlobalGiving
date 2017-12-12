53c1b09dcbf9cfcccbbe2af1228f2c2375e716a2
Steve Rogers <steve.rogers@envieta.com>
2017-12-12 16:58:05 -0500

    Comments and README.md

--- GlobalGiving/Extensions.swift
+++ GlobalGiving/Extensions.swift
@@ -14,18 +14,26 @@ import AlamofireImage
 var imageCache:[Int:UIImage] =  [:]
 
 extension UIImageView {
+    /**
+     Extension of UIImageView to enable the UIImageView component to load itself from a URL
+    */
     func downloadImageFrom(url: String, projectId: Int, contentMode: UIViewContentMode) {
         Alamofire.request(url).responseImage { response in
+            //callback/completion handler to handle response asynchronously
             switch response.result {
             case .success:
                 if let image = response.result.value {
+                    // cache image
                     imageCache[projectId] = image
+                    // and assign it to self's image attribute
                     self.image = image
                 } else {
                     print("Could not retrieve image from response...")
+                    // TODO should any action be taken, such as a default image being displayed (a red X?)
                 }
             case .failure(let error):
                 print("Error requesting '\(url)': \(error)")
+                // TODO should any action be taken, such as a default image being displayed (a red X?)
             }
     }
     }
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# GlobalGiving
+# GlobalGiving iOS Application
+
+## Requirements
+
+- XCode 9.x
+- Swift 4.x
+- [HomeBrew](http://brew.sh/)
+- [Carthage](https://github.com/Carthage/Carthage) Installed via HomeBrew
+- [AlamoFire](https://github.com/Alamofire/Alamofire) Installed via Carthage in Xcode Project
+- [AlamoFireImage](https://github.com/Alamofire/AlamofireImage) Installed via Carthage in Xcode Project
+
+## Project Setup
+
+- Clone/Fork this [repo](https://github.com/srogers2112/GlobalGiving)
+- Install Carthage using `brew install carthage` (requires [HomeBrew](http://brew.sh/))
+- In a terminal Shell, navigate to the project root folder
+  where the Cartfile is located and run `carthage update`
+  
